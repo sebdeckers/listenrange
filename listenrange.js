402fcadd65392575827a5fee262ed517f747b818
Sebastiaan Deckers <cbas@pandion.be>
2012-07-16 13:11:00 +0800

    Creating command line network debugging utility to listen on many ports

---
+++ listenrange.js
@@ -0,0 +1,41 @@
+var net = require('net'),
+	ranges = process.argv.slice(2),
+	callback = {
+		connection: function (socket) {
+			socket.setEncoding('utf8');
+			socket.on('data', callback.data.bind(socket));
+		},
+		listening: function () {
+			var host = this.address();
+			// console.log('listening on %s:%s', host.address, host.port);
+		},
+		error: function (error) {
+			console.log('Error: %s on port %s', error.code, this.port);
+		},
+		data: function (data) {
+			console.log('Port %s: %s', this.address().port, data);
+		}
+	},
+	parseRange = function (range) {
+		var format = /(\d+)(?:-(\d+))?/, // "n-m" or just "n"
+			matches = range.trim().match(format),
+			min = +matches[1],
+			max = +matches[2] || min,
+			valid = !isNaN(min) && !isNaN(max) && max >= min;
+		if (!valid) console.log('Warning: Invalid input: %s', range);
+		return valid ? { min: min, count : max - min } : range;
+	},
+	listenOnRange = function (ports) {
+		console.log('Setting up listeners %s-%s ...', ports.min, ports.min + ports.count);
+		for (var i = ports.min; i < ports.min + ports.count; i++) {
+			var server = net.createServer();
+			server.on('error', callback.error.bind({ port: i }));
+			server.on('connection', callback.connection.bind(server));
+			server.listen(i, callback.listening.bind(server));
+		}
+	};
+
+ranges
+	.map(parseRange)
+	.filter( function (range) { return typeof range !== 'string'; })
+	.forEach(listenOnRange);
---
+++ readme.md
@@ -0,0 +1,22 @@
+listenrange
+==
+
+Simple tool to listen on lots of ports. Couldn't find a command to do this so I rolled my own.
+
+## Installation
+
+Grab it off NPM.
+
+``` bash
+npm install -g listenrange
+```
+
+## Usage
+
+``` bash
+listenrange 21 80 500-700 1000-2000
+```
+or
+``` bash
+node listenrange.js 21 80 500-700 1000-2000
+```
